62b3bdcebad4d2d8814ae08a1c1b529a2a4db602
JonatanESalinas <A01731815@itesm.mx>
2021-10-09 14:05:52 -0500

    Added interpolation functionality #5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4;
+  bntText: string = 'Add an item';
+  goalText: string = 'My first life goal';
+  goals: string[] = [];
+
   constructor() { }
 
-  ngOnInit(): void {
+  //Life-cycle hook which is initiated when the app loads.
+  //cuando el componente se carga, se ejecuta esto:
+  ngOnInit() {
+    this.itemCount = this.goals.length;
   }
 
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
 }
